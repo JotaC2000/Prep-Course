cfe4129978b0ad5c7983670bd3463c79691ec7f0
JotaC2000 <cruzjohan@gmail.com>
2022-03-10 21:50:17 -0300

    25/35

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Hola soy un string";
+const nuevaString = "Aca hay un string";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 5;
